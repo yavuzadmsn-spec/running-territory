a8fbe4d6c703739a4242bcfb9b635ac659dd4c58
yavuzadmsn <yavuzadmsn@gmail.com>
2026-05-07 05:28:12 +0300

    feat: initial project setup with Supabase + Mapbox deps

---
+++ lib/supabase/server.ts
@@ -0,0 +1,18 @@
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export async function createClient() {
+  const cookieStore = await cookies()
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll: () => cookieStore.getAll(),
+        setAll: (cs) => cs.forEach(({ name, value, options }) =>
+          cookieStore.set(name, value, options)
+        )
+      }
+    }
+  )
+}
